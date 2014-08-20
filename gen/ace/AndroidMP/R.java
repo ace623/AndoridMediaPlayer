ef3b853c07b53c178b81ac5599513036fb71ccdb
U-ACE-VAIO\ACE <ACE@ACE-VAIO.(none)>
2014-08-20 15:50:35 +0800

    2

--- gen/ace/AndroidMP/R.java
+++ gen/ace/AndroidMP/R.java
@@ -1077,9 +1077,18 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int background=0x7f020057;
+        public static final int list=0x7f020058;
+        public static final int logo=0x7f020059;
+        public static final int next=0x7f02005a;
+        public static final int pause=0x7f02005b;
+        public static final int previous=0x7f02005c;
+        public static final int random=0x7f02005d;
+        public static final int repeat=0x7f02005e;
+        public static final int start=0x7f02005f;
     }
     public static final class id {
+        public static final int RelativeLayout1=0x7f05003c;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1091,7 +1100,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050049;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1105,6 +1114,7 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int handle_btnlayout=0x7f05003d;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1113,12 +1123,19 @@ containing a value of this type.
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
+        public static final int music_about_layout=0x7f050046;
+        public static final int music_album=0x7f050045;
+        public static final int music_list=0x7f050043;
         public static final int never=0x7f050009;
+        public static final int next_music=0x7f050042;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int play_music=0x7f050040;
+        public static final int previous_music=0x7f05003e;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int repeat_music=0x7f05003f;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1133,9 +1150,13 @@ containing a value of this type.
         public static final int showCustom=0x7f050007;
         public static final int showHome=0x7f050004;
         public static final int showTitle=0x7f050006;
+        public static final int shuffle_music=0x7f050041;
+        public static final int singleSong_layout=0x7f050044;
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int text1=0x7f050047;
+        public static final int text2=0x7f050048;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1239,9 +1260,10 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
+        public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int singer=0x7f0a000f;
+        public static final int time=0x7f0a0010;
     }
     public static final class style {
         /** 
--- src/ace/AndroidMP/MP.java
+++ src/ace/AndroidMP/MP.java
@@ -12,6 +12,7 @@ public class MP extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mp);
+
     }
 
 
